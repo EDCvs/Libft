6fd1eca9d7ca1c21fe0c1f61926fa9aa5650f08f
Iddy Chimvises <ichimvis@z2t3c3.42bangkok.com>
2026-09-15 17:26:38 +0700

    added added ft_bzero.c

---
+++ ft_bzero.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ichimvis <ichimvis@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/15 13:21:09 by ichimvis          #+#    #+#             */
+/*   Updated: 2026/09/15 17:07:57 by ichimvis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+PROTOTYPE
+----------
+       void ft_bzero(void *s, size_t n);
+
+RETURNS
+----------
+       None.
+
+DESCRIPTION
+----------
+       The  bzero()  function  erases  the  data  in the n bytes of the memory
+       starting at the location pointed to by s, by writing zeros (bytes  con‐
+       taining '\0') to that area.
+*/
+
+void ft_bzero(void *s, size_t n)
+{
+    ft_memset(s, 0, n);
+}
